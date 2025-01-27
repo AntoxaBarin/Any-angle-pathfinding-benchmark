15346c1a884162dfa7ebd7e79d1497932b887efc
Ivan Shanygin <shanygin101@yandex.ru>
2025-01-27 17:45:40 +0300

    Update A* CPP time results

--- astar/src/main.cpp
+++ astar/src/main.cpp
@@ -3,11 +3,6 @@
 #include "test.h"
 
 int main(int argc, char ** argv) {
-  // if (argc > 1 && strcmp(argv[1], "--test") == 0) {
-  //   return test_path_costs();
-  // }
-  // benchmark_grid_costs();
-
   test_path_costs("AR0500SR");
   test_path_costs("maze512-2-5");
   test_path_costs("Milan_1_1024");
