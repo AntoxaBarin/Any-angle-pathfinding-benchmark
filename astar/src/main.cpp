13b5dc46bdb076736347af1502b5319baf0b106e
Ivan Shanygin <shanygin101@yandex.ru>
2025-01-27 19:23:17 +0300

    Add A* CPP build & benchmark script

--- astar/src/main.cpp
+++ astar/src/main.cpp
@@ -3,11 +3,17 @@
 #include "test.h"
 
 int main(int argc, char ** argv) {
-  test_path_costs("AR0500SR");
-  test_path_costs("maze512-2-5");
-  test_path_costs("Milan_1_1024");
-  test_path_costs("random512-20-0");
+  if (argc != 2) {
+    std::cerr << "Incorrect number of command line args.\n Example: ./main <map-name>\n";
+    return -1;
+  }
 
+  test_path_costs(argv[1]);
+  
+  //test_path_costs("AR0500SR");
+  // test_path_costs("maze512-2-5");
+  // test_path_costs("Milan_1_1024");
+  // test_path_costs("random512-20-0");
   return 0;
 }
 
